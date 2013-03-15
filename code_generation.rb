e9ed308fac33a404b701dcd5db0f854fd6df1a17
Reiko Watanabe <wreikow9412@hotmail.com>
2013-03-15 20:57:42 +0100

    "creates a class from Title" test added

--- code_generation.rb
+++ code_generation.rb
@@ -1,10 +1,12 @@
 module Model
 
 	def self.generate()
-	#f= File.open
-	#l = f.readlines
-	#l.map{ |e| e.split}.select{|el| el[0] =~/title/}.flatten[1].gsub(":","")
 	
+		f= File.open('person.txt')
+		l = f.readlines
+		class_name = l.map{ |e| e.split}.select{|el| el[0] =~/title/}.flatten[1].gsub(":","")
+	  class_name = Object.const_set(class_name, Class.new)
+		#class_name.class
 	end
 
 end
--- test/code_generation_spec.rb
+++ test/code_generation_spec.rb
@@ -7,4 +7,8 @@ describe Model do
 	it "has a method generate" do
 		Model.must_respond_to("generate")
 	end
+	
+	it "creates a class from Title" do
+		Model.generate.must_be_kind_of(Class)
+	end
 end
