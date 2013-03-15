8416ebacb68a10fce0157405f309a33f3ae28d23
Reiko Watanabe <wreikow9412@hotmail.com>
2013-03-15 01:09:03 +0100

    code_generation.rb added

---
+++ code_generation.rb
@@ -0,0 +1,10 @@
+module Model
+
+	def self.generate()
+	#f= File.open
+	#l = f.readlines
+	#l.map{ |e| e.split}.select{|el| el[0] =~/title/}.flatten[1].gsub(":","")
+	
+	end
+
+end
--- test/code_generation_spec.rb
+++ test/code_generation_spec.rb
@@ -1,2 +1,10 @@
 require 'minitest/spec'
 require 'minitest/autorun'
+require 'minitest/pride'
+require_relative './../code_generation'
+
+describe Model do
+	it "has a method generate" do
+		Model.must_respond_to("generate")
+	end
+end
