369691040a27fa2e4ca192dc07830c734831d6fb
Reiko Watanabe <wreikow9412@hotmail.com>
2013-03-16 23:01:27 +0100

    Extract attributes from txt file

--- code_generation.rb
+++ code_generation.rb
@@ -1,12 +1,21 @@
 module Model
 
-	def self.generate()
-	
-		f= File.open('person.txt')
+	def self.generate(file_path)
+	  file_name = File.basename(file_path)
+		f= File.open()
 		l = f.readlines
 		class_name = l.map{ |e| e.split}.select{|el| el[0] =~/title/}.flatten[1].gsub(":","")
 	  class_name = Object.const_set(class_name, Class.new)
-		#class_name.class
+	  
+	  #find attributes:
+	  #l.map{|e| e.split}.select{|el| el[0] =~/attribute/}.map{|ele|ele[1].gsub(",","").gsub(":","").to_sym}
+
 	end
+
+=begin
+	def extract_title_as_classname(array_of_lines)
+		array_of_lines.map{ |e| e.split}.select{|el| el[0] =~/title/}.flatten[1].gsub(":","")
+	end
+=end
 
 end
